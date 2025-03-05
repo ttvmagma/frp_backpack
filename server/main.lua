2a3577ae44d95833d3dded54d56ad7ec69392366
ttvmagma <93156341+ttvmagma@users.noreply.github.com>
2025-03-05 13:47:54 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,39 +1,41 @@
 RegisterServerEvent('frp_backpack:setWeight')
 AddEventHandler('frp_backpack:setWeight', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    if not xPlayer then 
-        return 
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    if xPlayer then
+        MySQL.Async.fetchScalar('SELECT backpack FROM users WHERE identifier = @identifier', {
+            ['@identifier'] = xPlayer.identifier
+        }, function(backpackLevel)
+            local newWeight = 0
+            if backpackLevel then
+                if tonumber(backpackLevel) > 0 then
+                    newWeight = FUTURE.backpacklvl[tonumber(backpackLevel)] or 0
+                else
+                    newWeight = ESX.GetConfig().MaxWeight
+                end
+            end
+            xPlayer.setMaxWeight(newWeight)
+            TriggerClientEvent('frp_hud:notify', _source, "info", "Information", ('Inventargröße auf %s gesetzt'):format(newWeight), 5000)
+        end)
     end
-
-    MySQL.Async.fetchScalar('SELECT backpack FROM users WHERE identifier = @identifier', {
-        ['@identifier'] = xPlayer.identifier
-    }, function(backpackLevel)
-        backpackLevel = tonumber(backpackLevel) or 0
-        local newWeight = FUTURE.backpacklvl[backpackLevel] or ESX.GetConfig().MaxWeight
-
-        xPlayer.setMaxWeight(newWeight)
-        TriggerClientEvent('frp_hud:notify', source, "info", "Inventar", ('Inventargröße auf %s gesetzt'):format(newWeight), 5000)
-    end)
 end)
 
 RegisterServerEvent('frp_backpack:updateBackpack')
 AddEventHandler('frp_backpack:updateBackpack', function(playerId, backpackLevel)
     local xPlayer = ESX.GetPlayerFromId(playerId)
-    if not xPlayer then
-        print(('Spieler-ID %s nicht gefunden'):format(playerId))
-        return
+    if xPlayer then
+        MySQL.Async.execute('UPDATE users SET backpack = @backpack WHERE identifier = @identifier', {
+            ['@backpack'] = backpackLevel,
+            ['@identifier'] = xPlayer.identifier
+        }, function(rowsChanged)
+            if rowsChanged > 0 then
+                TriggerClientEvent('frp_backpack:neuerBackpack', playerId)
+                TriggerEvent('frp_backpack:setWeight', playerId)
+            end
+        end)
+    else
+        print(('Die Spieler-ID %s wurde nicht gefunden'):format(playerId))
     end
-
-    MySQL.Async.execute('UPDATE users SET backpack = @backpack WHERE identifier = @identifier', {
-        ['@backpack'] = backpackLevel,
-        ['@identifier'] = xPlayer.identifier
-    }, function(rowsChanged)
-        if rowsChanged > 0 then
-            TriggerClientEvent('frp_backpack:neuerBackpack', playerId)
-            TriggerClientEvent('frp_backpack:loadBackpack', playerId)
-            TriggerEvent('frp_backpack:setWeight', playerId)
-        end
-    end)
 end)
 
 for level, itemName in pairs(FUTURE.backpackItems) do
@@ -48,6 +50,7 @@ for level, itemName in pairs(FUTURE.backpackItems) do
         if item.count > 0 then
             xPlayer.removeInventoryItem(itemName, 1)
             TriggerEvent('frp_backpack:updateBackpack', playerId, level)
+            TriggerClientEvent("frp_backpack:loadBackpack", playerId)
         else
             TriggerClientEvent('frp_hud:notify', playerId, "error", "Inventar", "Du hast keinen Rucksack!", 5000)
         end
